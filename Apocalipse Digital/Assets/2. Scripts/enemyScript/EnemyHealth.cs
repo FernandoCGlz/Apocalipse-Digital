3897286a148579649321b811a189738f35a5375f
FrnoXz <fernandocamarena@jerez.es>
2025-04-14 21:04:05 +0200

    14/04/2025

--- Apocalipse Digital/Assets/2. Scripts/enemyScript/EnemyHealth.cs
+++ Apocalipse Digital/Assets/2. Scripts/enemyScript/EnemyHealth.cs
@@ -1,8 +1,10 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
     Enemy enemy;
+    public bool isDamaged;
     public GameObject  deathEffect;
     private void Start()
     {
@@ -11,13 +13,21 @@ public class EnemyHealth : MonoBehaviour
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Weapon")){
+        if(collision.CompareTag("Weapon") &&!isDamaged){
             enemy.healthpoints-=2f;
+            StartCoroutine(Damager());
             if(enemy.healthpoints <=0){
                 Instantiate(deathEffect, transform.position, Quaternion.identity);
                 Destroy(gameObject);
             }
         }
     }
+    IEnumerator Damager()
+{
+    isDamaged = true;
+    yield return new WaitForSeconds(0.5f);
+    isDamaged = false;
+
+}
 
 }
